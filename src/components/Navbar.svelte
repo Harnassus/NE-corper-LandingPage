0a1b38e04f7c87c53798056080cb757ed42517f9
Michael Festus <micfestus45@gmail.com>
2022-10-07 20:30:02 +0100

    started work on the layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="w-screen">
+    <div>
+        <h1>NE</h1>
+    </div>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Aclonica&display=swap');
+
+    h1{
+        font-family: 'Aclonica', sans-serif;
+    }
+</style>
